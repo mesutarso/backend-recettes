4be58dbbfbc79512d4f2086c69c9b399f4d851ce
mechack tambwe <mesutarso@users.noreply.github.com>
2024-06-18 16:18:41 +0100

    init commit

---
+++ config/api.js
@@ -0,0 +1,7 @@
+module.exports = {
+  rest: {
+    defaultLimit: 2500,
+    maxLimit: 10000,
+    withCount: true,
+  },
+};
---
+++ config/plugins.js
@@ -0,0 +1,23 @@
+module.exports = ({ env }) => ({
+  //...
+  "import-export-entries": {
+    enabled: true,
+    config: {
+      // See `Config` section.
+    },
+  },
+  transformer: {
+    enabled: true,
+    config: {
+      responseTransforms: {
+        removeAttributesKey: true,
+        removeDataKey: true,
+      },
+      requestTransforms: {
+        wrapBodyWithDataKey: true,
+      },
+    },
+  },
+  
+  //...
+});
